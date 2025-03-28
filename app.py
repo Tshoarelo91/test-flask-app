93ddbcfba107669087fd722c1d1199c521dac1c8
Tshoarelo Moleke <Moleketk@gmail.com>
2025-03-28 22:29:05 +0200

    Simplified for PythonAnywhere

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,6 +6,6 @@ app = Flask(__name__)
 def hello():
     return 'Hello from Flask!'
 
+# This is only used when running locally
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
