43b7a586ffa5b266659b287a57288092a9e8dab0
Tshoarelo Moleke <Moleketk@gmail.com>
2025-03-28 21:50:16 +0200

    Added basic todo API

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
+# Simple in-memory storage for now
+todos = []
 
 @app.route('/')
 def hello():
-    return 'Hello from Flask!'
+    return 'Todo API is running!'
+
+@app.route('/api/todos', methods=['GET'])
+def get_todos():
+    return jsonify(todos)
+
+@app.route('/api/todos', methods=['POST'])
+def add_todo():
+    todo = request.json
+    todos.append(todo)
+    return jsonify(todo), 201
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
