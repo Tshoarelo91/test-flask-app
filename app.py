64064d88e78b8db1203164b9434bf9b60a37fc44
Tshoarelo Moleke <Moleketk@gmail.com>
2025-03-28 22:26:46 +0200

    Simplified to bare minimum

--- app.py
+++ app.py
@@ -1,25 +1,12 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+import os
+from flask import Flask
 
 app = Flask(__name__)
-CORS(app)
-
-# Simple in-memory storage for now
-todos = []
 
 @app.route('/')
 def hello():
-    return 'Todo API is running!'
-
-@app.route('/api/todos', methods=['GET'])
-def get_todos():
-    return jsonify(todos)
-
-@app.route('/api/todos', methods=['POST'])
-def add_todo():
-    todo = request.json
-    todos.append(todo)
-    return jsonify(todo), 201
+    return 'Hello from Flask!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
